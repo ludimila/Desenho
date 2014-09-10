3769b651091ddd4ba77df3b63bf9a9b24cad64b2
ParleyMartins <parleypachecomartins@gmail.com>
2014-09-10 11:24:00 -0300

    Adicionada criacao de usuario

--- SOMA/app/controllers/users_controller.rb
+++ SOMA/app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if(@user.save)
+      flash[:success] = "Sua conta será ativada assim que recebermos a confirmação do seu pagamento."
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :rg, :issuing_agency, :cpf, :phone1, :phone2, :zip_code, :email) 
+    end
 end
