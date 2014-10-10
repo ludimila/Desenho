0badb948baee160f17c802239b46ee834bc601c0
ParleyMartins <parleypachecomartins@gmail.com>
2014-10-10 17:52:30 -0300

    Secret Token removido do repositorio publico e adicionado no .gitignore

--- SOMA/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SOMA::Application.config.secret_token = '6f13437d435b21d1f18b478c4a218b1c46aaf3f5b53bb8659d64f8f852460879adee957323d0c9f1054bd2fe6047e921d880e6e5618a08c7d2716ef083f6b6fb'
