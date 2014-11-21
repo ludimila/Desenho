dc23ead2b59ccec299341e27cf32a34e208afce3
Macario Soares <macario.junior@gmail.com>
2014-11-21 20:48:41 -0200

    Ajeitando classes modelo para comportamento correto com o documento

--- SOMA/app/models/document.rb
+++ SOMA/app/models/document.rb
@@ -1,3 +1,24 @@
 class Document < ActiveRecord::Base
+  before_destroy :destroy_documents
   belongs_to :course, inverse_of: :documents
+
+  SECRET_NAME = Array.new(5){[*"A".."Z", *"0".."9"].sample}.join
+  has_attached_file :document ,{
+    url: "/system/:hash.:extension",
+    hash_secret: SECRET_NAME,
+    styles: {
+      pdf_thumbnail: ["100x100#", "jpg"],
+      :small  => ["150x150>", "jpg"],
+      :medium => ["200x200", "jpg"] 
+    }
+  }
+  validates_attachment :document, :presence => true,
+    :content_type => { :content_type => "application/pdf" },
+    :size => { :in => 0..5.megabytes }
+  validates_attachment_file_name :document, :matches => [/pdf\Z/]
+
+  private
+    def destroy_documents
+      self.document.destroy
+    end
 end
