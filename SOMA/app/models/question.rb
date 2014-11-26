4661d5ed13ed19f546e80f662bc0dda0fce2b6a9
Ludimila <lbelacruz@gmail.com>
2014-11-26 19:36:51 -0200

    Classes de Questão ajeitadas

--- SOMA/app/models/question.rb
+++ SOMA/app/models/question.rb
@@ -1,3 +1,13 @@
 class Question < ActiveRecord::Base
   belongs_to :course, inverse_of: :questions
+  has_many :question_options
+
+  validates :right_answer, presence: true, numericality: { only_integer: true }
+  validates :command, presence: true
+
+  accepts_nested_attributes_for :question_options
+
+  def right_answer?(answer)
+    self.right_answer == answer
+  end
 end
