68899e50f9428f9ab969f8388dbac8469e8d3ace
Ludimila <lbelacruz@gmail.com>
2014-11-28 14:42:52 -0200

    Adicionando views iniciais de video

--- SOMA/app/models/video.rb
+++ SOMA/app/models/video.rb
@@ -1,4 +1,11 @@
 class Video < Resource
+
+  alias_attribute :full_url, :document_file_name
+  alias_attribute :youtube_id, :document_content_type
+
+  URL_REGEX = /\A(?:https?:\/\/)?(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=)?([\w-]{10,})\z/i
+  validates :desciption, format: { with: URL_REGEX }
+
   def video?
     true
   end
