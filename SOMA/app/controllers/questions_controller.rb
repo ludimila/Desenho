7d71fa0447ed75091c203855eeeb99f02d089b7a
Ludimila <lbelacruz@gmail.com>
2014-11-28 10:47:30 -0200

    Adicionando Template Method à questoes

--- SOMA/app/controllers/questions_controller.rb
+++ SOMA/app/controllers/questions_controller.rb
@@ -1,32 +1,45 @@
-class QuestionsController < ApplicationController
-  include ApplicationHelper
-  before_action :get_menu
-
+class QuestionsController < ResourcesController 
   def new
-    @question = Question.new
+    @resource = Question.new
     answers = Array.new
-    @question.options_amount.times do |a|
+    @resource.options_amount.times do |a|
       a = QuestionOption.new
       answers << a
     end
-    @question.question_options = answers
-  end
-
-  def create
-    @question = Question.create(question_params)
-    render "new"
-  end
-
-  def index
+    @resource.question_options = answers
   end
 
   private
     def question_params
-      params.require(:question).permit(:command, :right_answer, :question_options_attributes)
-    end
-    
-    def get_menu
-      @MENU_OPTIONS = {questions: 4}
+      params.require(:question).permit(:description)
     end
 
+    def notify_resource_destruction(resource)
+      QuestionObserver.notify_destruction(current_course, resource)
+    end
+
+    def redirect_to_resources_page
+      redirect_to questions_path and return
+    end
+
+    def delete_other_resources_from_list(resource)
+      @resources.delete(resource) if !resource.question?
+    end
+
+    def identify_resource
+      @resource = Question.new(question_params)
+    end
+
+    def find_repeated_resource
+      false
+    end
+
+    def notify_resource_creation(resource)
+      QuestionObserver.notify_creation(current_course, resource)
+      flash[:success] = "Questão adicionada com sucesso."
+    end
+
+    def find_resource
+      @resource = Question.find(params[:id])
+    end
 end
--- SOMA/app/models/question.rb
+++ SOMA/app/models/question.rb
@@ -2,7 +2,7 @@ class Question < Resource
   has_many :question_options
 
   validates :right_answer, presence: true, numericality: { only_integer: true }
-  validates :command, presence: true
+  validates :description, presence: true
 
   accepts_nested_attributes_for :question_options
 
